c09cc2ad7d785597f33ea0cf2be448b483ab3697
linxiaoruxr <linxiaoruxr@163.com>
2017-11-28 15:10:23 +0800

    add list.py

---
+++ examples/basic/list.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+L = [['Apple', 'Google', 'Microsoft'],
+['Java', 'Python', 'Ruby', 'PHP'],
+['Adam', 'Bart', 'Lisa']]
+# 打印 Apple：
+print(L[0][0])
+# 打印 Python：
+print(L[1][1])
+# 打印 Lisa：
+print(L[2][2])
