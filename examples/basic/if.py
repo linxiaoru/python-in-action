c5923b254e22a313bbd8c9d85481bb7d8e646a9c
linxiaoruxr <linxiaoruxr@163.com>
2017-11-28 16:35:07 +0800

    update if.py

--- examples/basic/if.py
+++ examples/basic/if.py
@@ -3,11 +3,11 @@ height = 1.75
 weight = 80.5
 bmi = weight/(height*height)
 print(bmi)
-if bmi < 18.5:
+if bmi <= 18.5:
     print('过轻')
-elif bmi < 25:
+elif bmi <= 25:
     print('正常')
-elif bmi < 28:
+elif bmi <= 28:
     print('过重')
 else:
     print('肥胖')
