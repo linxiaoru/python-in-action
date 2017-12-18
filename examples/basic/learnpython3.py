ed7725ca138568834a7987689a35f8d429d62ab1
林小如 <linxiaoruxr@163.com>
2017-12-18 18:17:31 +0800

    Update learnpython3.py

--- examples/basic/learnpython3.py
+++ examples/basic/learnpython3.py
@@ -237,7 +237,8 @@ tup = (1, 2, 3)
 tup[0]          # => 1
 tup[0] = 3      # 抛出 TypeError（类型错误） 异常
 
-# 注意：长度为 1 的元组应该在元组的最后一位元素后加上一个逗号，但是长度不为 1 的元组不应该在末位元素后面加逗号，即使是长度为 0 的也不要加
+# 注意：长度为 1 的元组应该在元组的最后一位元素后加上一个逗号，
+# 但是长度不为 1 的元组不应该在末位元素后面加逗号，即使是长度为 0 的也不要加
 type((1))   # => <class 'int'>
 type((1,))  # => <class 'tuple'>
 type(())    # => <class 'tuple'>
@@ -250,3 +251,227 @@ tup[:2]             # => (1, 2)
 
 # 可以把元组（或列表）中的元素取出来赋值给多个变量
 a, b, c = (1, 2, 3) # a 现在是 1，b 是 2，c 是 3
+# 也可以进行序列解包，就是把首个或前几个元素与后面几个元素分别提取出来
+a, *b, c = (1, 2, 3, 4) # a 现在是 1，b 现在是 [2, 3]，c 是 4
+# 如果省略了括号，那么元组会被自动创建
+d, e, f = 4, 5, 6
+# 来看看交换两个值有多么容易
+e, d = d, e     # d 现在是 5，e 是 4
+
+
+# 字典用于存储键值对的映射关系
+empty_dict = {}
+# 预先填充的字典
+filled_dict = {"one": 1, "two": 2, "three": 3}
+
+# 注意：dict 的 key 必须是不可变对象。这是因为 dict 根据 key来计算 value 的存储位置，
+# 如果每次计算相同的 key 得出的结果不同，那dict内部就完全混乱了。
+# 这个通过 key 计算位置的算法称为哈希算法（Hash），使得 dict 可以进行高速查找。
+# 要保证 hash 的正确性，作为 key 的对象就不能变。
+# 在 Python 中，字符串、整数、浮点数、元组等都是不可变的，因此，可以放心地作为 key。而 list 是可变的，就不能作为 key。
+invalid_dict = {[1, 2, 3]: "123"}       # 抛出一个类型错误：不可进行哈希计算的类型：'list'
+valid_dict = {(1, 2, 3): [1, 2, 3]}     # 值可以是任何类型的
+
+# 使用 [] 来查找值
+filled_dict["one"]  # => 1
+
+# 使用 keys() 方法将所有的 key 作为一个可遍历的对象来获取。我们需要用 list() 来包裹，将其转化为列表。
+# 注意：字典的键的顺序是不能保证的，你得到的结果可能和这个示例不一致
+list(filled_dict.keys())    # => ["three", "two", "one"] 
+
+# 使用 values() 方法将所有的 value 作为一个可遍历的对象来获取。同样，我们将其用 list() 来包裹，将其转化为列表。
+# 注意：关于他们的顺序问题和上面 key 顺序问题一样，都是随机的。
+list(filled_dict.values())  # => [3, 2, 1]
+
+# 检查字典中键的存在性问题使用 in
+"one" in filled_dict        # => True
+1 in filled_dict            # => False
+
+# 查找一个不存在的 key 会产生键名错误
+filled_dict["four"]         # keyError,键名错误
+
+# 使用 get() 方法来避免键名错误
+# filled_dict.get("one")    # => 1
+# filled_dict.get("four")   # => None
+# get() 方法支持传入一个默认参数，当 value 不存在时返回这个默认参数
+filled_dict.get("one", 4)   # => 1
+filled_dict.get("four", 4)  # => 4
+
+# setdefault() 方法可以将一个键值对插入字典，只有当这个键在原先的字典中不存在时成立
+filled_dict.setdefault("five", 5)   # filled_dict["five"] 被设置成 5 
+filled_dict.setdefault("five", 6)   # filled_dict["five"] 仍旧是 5
+
+# 添加到字典
+filled_dict.update({"four":4})      # => {"one": 1, "two": 2, "three": 3, "five": 5, "four": 4}
+filled_dict["four"] = 4             # 另一种添加到字典的方法
+
+# 使用 del 来移除字典的键
+del filled_dict["one"]      # 从填充好的字典中移除 "one" 键
+
+# Python 3.5 还可以使用附加的解包选项
+{'a': 1, **{'b': 2}}    # => {'a': 1, 'b': 2}
+{'a': 1, **{'a': 2}}    # => {'a': 2}
+
+# set 用于保存集合
+empty_set = set()
+# 使用一连串的值来初始化一个 set 集合。是的，看起来和一个字典很像。
+# 然而，set 中的键是不能重复的
+some_set = {1, 1, 2, 2, 3, 4}      # some_set 现在是 {1, 2, 3, 4}
+
+# 和字典的键累死，集合的元素也是不可变对象
+invalid_set = {[1], 1}      # => 抛出类型错误：不可进行哈希计算的类型：'list'
+valid_set = {(1,), 1}
+
+# 添加一个项到集合
+filled_set = some_set
+filled_set.add(5)       # filled_set 现在是 {1, 2, 3, 4, 5}
+
+# 使用 & 计算交集
+other_set = {3, 4, 5, 6}
+filled_set & other_set  # => {3, 4, 5}
+
+# 使用 | 计算并集
+filled_set | other_set  # => {1, 2, 3, 4, 5, 6}
+
+# 使用 - 计算补集
+{1, 2, 3, 4} - {2, 3, 5}    # => {1, 4}
+
+# 使用 ^ 计算对称差集
+{1, 2, 3, 4} ^ {2, 3, 5}    # => {1, 4, 5}
+
+# 检查左边的集合是否是右边集合的超集
+{1, 2} >= {1, 2, 3}         # => False
+
+# 检查左边的集合是否是右边集合的子集
+{1, 2} <= {1, 2, 3}         # => True
+
+# 使用 in 来检查一个元素是否存在于集合中
+2 in filled_set             # => True
+10 in filled_set            # => False
+
+
+
+####################################################
+## 3. 控制流和可迭代对象
+####################################################
+
+# 创建一个变量
+some_var = 5
+
+# 这有一个条件语句。缩进在 Python 中很重要！
+# 惯例上使用四个空格来锁进，而不是 tab 缩进
+# 这会打印出 "some_var is smaller than 10"
+if some_var > 10:
+    print("some_var is totally bigger than 10.")
+elif some_var < 10:     # 这里 elif 分句是可选的
+    print("some_var is smaller than 10.")
+else:
+    print("some_var is indeed 10.")
+
+"""
+for 循环打印列表：
+打印:
+    dog is a mammal
+    cat is a mammal
+    mouse is a mammal
+"""
+for animal in ["dog", "cat", "mouse"]:
+    # 可以使用 format() 来格式化字符串
+    print("{} is a mammal".format(animal))
+
+"""
+使用 "range(number)" 遍历一个数字序列，范围从 0 到给定的数
+打印：
+    0
+    1
+    2
+    3
+"""
+for i in range(4):
+    print(i)
+
+"""
+使用 "range(lower, upper)" 遍历一个数字序列，范围从第一个小的参数到第二个大的参数（包括第一个参数，不包括第二个参数）
+打印：
+    4
+    5
+    6
+    7
+"""
+for i in range(4, 8):
+    print(i)
+
+"""
+使用 "range(lower, upper, step)" 遍历一个数字序列，范围从第一个小的参数到第二个大的参数,
+间隔第三个参数的位数，例如 step = 2，则意味间隔 2 个位数，当没有指定 step 的值时，默认值为 1。
+打印：
+    4
+    6
+"""
+for i in range(4, 8, 2):
+    print(i)
+
+"""
+while 循环会一直持续循环直到条件不再满足为止。
+打印：
+    0
+    1
+    2
+    3
+"""
+x = 0
+while x < 4:
+    print(x)
+    x += 1      # x = x + 1 的简写方式
+
+# 使用 try/except 块来处理异常
+try:
+    # 使用 raise 来抛出一个错误
+    raise IndexError("This is an index error")
+except IndexError as e:
+    pass                # pass 只是一个空操作。通常你应该在这里做一些恢复工作。
+except (TypeError, NameError):
+    pass                # 如果需要的话，多个异常可以一起处理
+else:                   # try/except 块的可选分句，必须遵循所有的 except 块
+    print("All good!")  # 只有当代码在 try 中没有抛出异常时才运行这里
+finally:                # 在所有情况下都会执行
+    print("we can clean up resources here")
+
+# 可以使用 with 语句替换 try/finally 来进行资源清理
+with open("myfile.txt") as f:
+    for line in f:
+        print(line)
+
+# Python 提供了一个基础的抽象概念叫作迭代器
+# 可迭代对象可被当作一个序列来对待
+# range 函数返回的对象就是一个可迭代对象
+
+filled_dict = {"one": 1, "two": 2, "three": 3}
+our_iterable = filled_dict.keys()
+print(our_iterable)     # => dict_keys(['one', 'two', 'three']). 这就是一个实现了我们的可迭代对象接口的对象
+
+# 我们能够遍历它
+for i in our_iterable:
+    print(i)        # 打印 one, two, three
+
+# 然而我们不能通过元素索引来处理元素
+our_iterable[1]     # 抛出类型异常错误
+
+# 迭代器是一个知道如何创建可迭代对象的对象
+our_iterable = iter(our_iterable)
+
+# 迭代器时一个能够记住我们遍历它时的状态的对象
+# 迭代器是通过next()来实现的，每调用一次他就会返回下一个元素，
+# 当没有下一个元素的时候返回一个StopIteration异常，所以实际上定义了这个方法的都算是迭代器。
+next(our_iterable)      # => "one"
+
+# 当我们进行迭代时它能够维持状态
+next(our_iterable)      # => "two"
+next(our_iterable)      # => "three"
+
+# 当没有下一个元素的时候返回一个 StopIteration 异常
+next(our_iterable)      # 抛出 StopIteration 异常
+
+# 可以使用 list() 来获取迭代器中所有的元素
+list(filled_dict.keys())    # => 返回 ["one", "two", "three"]
+
