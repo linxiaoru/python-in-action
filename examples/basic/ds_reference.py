131c483041d58738d44f31ba82cb9c1f56d51c1f
林小如 <linxiaoruxr@163.com>
2018-01-03 15:39:57 +0800

    Add ds_reference.py

---
+++ examples/basic/ds_reference.py
@@ -0,0 +1,22 @@
+print('Simple Assignment')
+shoplist = ['apple', 'mango', 'carrot', 'banana']
+# mylist 只是指向同一对象的另一种名称
+mylist = shoplist
+
+# 我购买了第一项项目，所以我将其从列表中删除
+del shoplist[0]
+
+print('shoplist is', shoplist)
+print('mylist is', mylist)
+# shoplist 和 mylist 打印出同样的结果，apple 都没有了，
+# 它们指向的是同一个对象
+
+print('Copy by making a full slice')
+# 通过生成一份完整的切片制作一份列表的副本
+mylist = shoplist[:]
+# 删除第一个项目
+del mylist[0]
+
+print('shoplist is', shoplist)
+print('mylist is', mylist)
+# mylist 没有了 mango, shoplist 还存在 mango
